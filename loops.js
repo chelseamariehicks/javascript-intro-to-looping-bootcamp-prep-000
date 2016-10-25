e53899f5d391de7f0d110a40363e5485fce986ae
Chelsea Hicks <chelseamariehicks@gmail.com>
2016-10-25 19:26:13 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,13 @@
+function forLoop(array) {for( i = 0; i < 24; i++) {if (i === 1)
+  {return "I am 1 strange loop."} else {return "I am ${i} strange loops."}}
+return array};
+
+function whileLoop(n) {while (n > 0) {console.log(--n);
+
+if (n=0) {return "done"}};
+
+function doWhileLoop (array) {};
+
+do {
+  console.log()
+} while (array.length > 0 && maybeTrue());
